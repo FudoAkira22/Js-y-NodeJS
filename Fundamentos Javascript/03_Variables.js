de19fb4ffa7d5aca6e0ef54ae25e7ae1e6def28e
Erick Martin Medina Pantoja <medinapantoja1@gmail.com>
2024-02-13 22:29:28 -0600

    Mensaje descriptivo de los cambios realizados

---
+++ Fundamentos Javascript/03_Variables.js
@@ -0,0 +1,29 @@
+/* 
+Formas de definir variables
+-var:este es para que hacepte cualquier tipo de variables
+-let: solo se usa en el bloque de codigo no de forma global
+-const :este es para variables que nunca cambian
+*/
+
+var nombre = 'Erick';
+console.log(nombre);
+var edad = 22;
+console.log(edad);
+var ganancia = 2.22;
+console.log(ganancia);
+
+//Un objeto
+var persona ={
+    nombre:"David",
+    edad:18,
+    direccion:{
+        calle:'Siempre Viva',
+        nuemroDecasa:22
+    },
+    ciudadesPorVisitar:[
+        'GDL',
+        'CDM',
+        'Colima'
+    ]
+    }
+console.log(persona);
---
+++ Fundamentos Javascript/Ejercicios - Operadores/SegundosDeUnDia.js
@@ -0,0 +1,5 @@
+var segundos = 60;
+var min = 60;
+var diaH = 24;
+var op1 = (segundos * min) * diaH;
+console.log(op1);
